aefe40df50edce5696b2e4ffa2bf9b4672cf9546
Akihiro <marsstay0729@gmail.com>
2020-01-18 11:29:06 +0900

    [add]view messages save in the firebase

--- components/Chat.vue
+++ components/Chat.vue
@@ -1,33 +1,31 @@
 <template>
   <div class="content">
     <div>
-      <img :src="message.user.thumbnail" class="content__img" alt="画像" />
+      <img :src="user.thumbnail" class="content__img" alt="画像" />
     </div>
     <div>
       <div class="content__user-name">{{ displayName }}</div>
-      <div class="content__message">{{ message.comment }}</div>
+      <div class="content__message">{{ message.message }}</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  props: ["message"],
   data() {
     return {
-      message: {
-        comment: "コメントだお",
-        user: {
-          id: 1,
-          name: "akihiro",
-          thumbnail:
-            "https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg"
-        }
+      user: {
+        id: 1,
+        name: "akihiro",
+        thumbnail:
+          "https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg"
       }
     }
   },
   computed: {
     displayName() {
-      return `@${this.message.user.name}`
+      return `@${this.user.name}`
     }
   }
 }
--- components/ChatArea.vue
+++ components/ChatArea.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="contents">
     <!-- chat -->
-    <chat />
-    <chat />
-    <chat />
-    <chat />
-    <chat />
-    <chat />
-    <chat />
-    <chat />
-    <chat />
-    <chat />
+    <chat v-for="(message, id) in messages" :key="id" :message="message" />
   </div>
 </template>
 
 <script>
 import Chat from "./Chat"
+import { db } from "~/plugins/firebase"
 
 export default {
   components: {
     Chat
+  },
+  data() {
+    return {
+      messages: []
+    }
+  },
+  mounted() {
+    // [参考][パラメータ取得]https://qiita.com/tabtt3/items/cd4405c6ab1a05d3b2c3
+    const roomId = this.$route.params.id
+    db.collection("rooms").doc(roomId).collection("messages").get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        this.messages.push({id: doc.id, ...doc.data()})
+      });
+    });
   }
 }
 </script>
