13624bf87d9f07e4221aa8f4d659fa22f4b356b7
Akihiro <marsstay0729@gmail.com>
2020-01-23 18:06:32 +0900

    add room title in ContainHeader.vue

--- components/ContainerHeader.vue
+++ components/ContainerHeader.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="container__header">
-    <h2 class="title">ルーム名が入るよ！</h2>
+    <h2 class="title">{{ roomName }}</h2>
   </div>
 </template>
+
+<script>
+import { db } from "~/plugins/firebase"
+
+export default {
+  data() {
+    return {
+      roomName: null
+    }
+  },
+  mounted() {
+    // [参考]https://goq.to/MDA2ODIz
+  const roomId = this.$route.params.id
+  db.collection("rooms").doc(roomId).get()
+    .then(doc => {
+      this.roomName = doc.data().name
+    })
+  }
+}
+</script>
+
 
 <style lang="scss" scoped>
 .container__header {
